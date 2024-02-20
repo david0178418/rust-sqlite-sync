e6f8aedc28251fbef8a833d72a763112d905aa51
David Granado <davidjgranado@gmail.com>
2024-02-19 23:14:49 -0600

    Get proof of concept mdns discovery working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod discovery;
+
+use crate::discovery::{query, register};
+
 fn main() {
-	println!("Explore leveraging mDNS for discovery with sqlite + CRDTs extension for offline-first sync");
+	// Require a paramter of either "client" or "responder".
+	let args: Vec<String> = std::env::args().collect();
+	let bar = String::from("<blank>");
+	let role = args.get(1).unwrap_or(&bar);
+
+	if role != "client" && role != "responder" {
+		println!("Must provide a valid role 'client' or 'responder''");
+		std::process::exit(1);
+	}
+
+	if role == "client" {
+		println!("Running as client");
+		query(args[1..].to_vec().clone());
+	} else {
+		println!("Running as responder");
+		register(args[1..].to_vec().clone());
+
+		loop {
+			println!("Responder is running");
+			std::thread::sleep(std::time::Duration::from_millis(2000));
+		}
+	}
 }
