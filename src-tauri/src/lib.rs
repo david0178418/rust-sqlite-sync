0b87bb27d84cf1ac5915ae2bb3bd15ebdec48c2e
David Granado <davidjgranado@gmail.com>
2024-02-28 17:13:43 -0600

    Add Mui

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,17 @@
+use std::{thread::sleep, time::Duration};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
+#[tauri::command(async)]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+	sleep(Duration::from_secs(5));
+	format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+	tauri::Builder::default()
+		.plugin(tauri_plugin_shell::init())
+		.invoke_handler(tauri::generate_handler![greet])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
